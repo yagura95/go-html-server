9baa5e367d4c7e077f9bf13515b1497e5da75bf1
Joao Barbosa <joaob@evoke.pt>
2024-11-23 19:18:33 +0000

    base custom server

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import ( 
-  "fmt"
   "net/http"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Welcome home")
-    })
+    mux := http.NewServeMux()
 
-    http.ListenAndServe(":8080", nil)
+    s := &http.Server{
+      Addr: ":8080",
+      Handler: mux,
+    }
+
+    s.ListenAndServe()
 }
 
