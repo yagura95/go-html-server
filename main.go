95824171c1af3671f2b09b98e205c98c95d1d682
Joao Barbosa <joaob@evoke.pt>
2024-11-25 23:48:08 +0000

    adding state to app and api calls to modify it

--- main.go
+++ main.go
@@ -1,13 +1,51 @@
 package main
 
 import ( 
+  "fmt"
   "net/http"
+  "sync/atomic"
 )
 
+type apiConfig struct {
+  fileserverHits atomic.Int32
+}
+
+func (conf *apiConfig) hitsIncMiddleware(next http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { 
+    conf.fileserverHits.Add(1)   
+    w.Header().Set("Cache-Control", "no-cache")
+    next.ServeHTTP(w, r)
+  }) 
+}
+
+func (conf *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(200)
+  w.Write([]byte(fmt.Sprintf("%d", conf.fileserverHits.Load())))
+}
+
+func (conf *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+  conf.fileserverHits.CompareAndSwap(conf.fileserverHits.Load(), 0)
+
+  w.WriteHeader(200)
+} 
+
 func main() {
+    conf := apiConfig{}
+
     mux := http.NewServeMux()
 
-    mux.Handle("/", http.FileServer(http.Dir("."))) 
+    appFunc := http.StripPrefix("/app/", http.FileServer(http.Dir("./app")))
+
+    mux.Handle("/app/", conf.hitsIncMiddleware(appFunc))
+
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+      w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+      w.WriteHeader(200)
+      w.Write([]byte("Ok"))
+    })
+
+    mux.HandleFunc("/metrics", conf.metricsHandler)
+    mux.HandleFunc("/reset", conf.resetHandler)
 
     s := &http.Server{
       Addr: ":8080",
